25b9c8564ada0f35d0cb96d16c2be1471bd7d2c7
toroi-ex <toroigit@gmail.com>
2020-07-28 16:04:23 +0900

    121 A B add

--- atconder.py
+++ atconder.py
@@ -1,14 +1,34 @@
-from collections import Counter
+# from collections import Counter
 
-n, m = map(int, input().split())
-ka = []
-for i in range(n):
-    x = [int(y) for y in input().split()]
-    ka.extend(x[1:])
+# n, m = map(int, input().split())
+# ka = []
+# for i in range(n):
+#     x = [int(y) for y in input().split()]
+#     ka.extend(x[1:])
 
-count = 0
-for i,j in Counter(ka).items():
-    if j == n:
-        count += 1
-print(count)
+# count = 0
+# for i,j in Counter(ka).items():
+#     if j == n:
+#         count += 1
+# print(count)
 
+#121 A
+# H, W = map(int, input().split())
+# h, w = map(int, input().split())
+
+# print((H-h)*(W-w))
+
+#121 B
+# from operator import mul
+# n, m, c = map(int, input().split())
+# b = list(map(int, input().split()))
+# count = 0
+
+# for i in range(n):
+#     a = list(map(int, input().split()))
+#     com = map(mul, a, b)
+#     if sum(com) + c > 0:
+#         count += 1
+#     a.clear()
+
+# print(count)
