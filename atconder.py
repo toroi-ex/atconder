923022c3d07bd0ccb8ef364426ca466b21e7e8ec
toroi-ex <toroigit@gmail.com>
2020-07-31 14:39:28 +0900

    121 C add

--- atconder.py
+++ atconder.py
@@ -32,3 +32,22 @@
 #     a.clear()
 
 # print(count)
+
+#121 C
+# n, m = map(int, input().split())
+# ab = []
+# for i in range(n):
+#     AB = list(map(int, input().split()))
+#     ab.append(AB)
+# x = 0
+# val = 0
+# ab.sort(key=lambda x:x[0])
+# for i in range(n):
+#     x += ab[i][1]
+#     val += ab[i][0]*ab[i][1]
+#     if x < m:
+#         continue
+#     else:
+#         val = val - ab[i][0]*(x - m)
+#         break
+# print(val)
